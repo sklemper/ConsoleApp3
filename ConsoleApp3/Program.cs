d34078d634ecebbe662c2beea9f504c1c0d55ade
Danil <danilsonia621@gmail.com>
2020-11-03 15:00:09 +1000

    2.11.2020

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat("Мурзик", new DateTime(2009, 04, 29));
+            c.MakeNoise();
+            Console.WriteLine($"Коту {c.Name} уже {c.GetAge()} лет");
+        }
+    }
+}
---
+++ ConsoleApp3/cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Cat
+    {
+        public Cat(string name, DateTime berthday)
+        {
+
+            Name = name;
+            Berthday = berthday;
+
+
+        }
+
+        public string Name
+        {
+            get;
+
+            set;
+        }
+        public DateTime Berthday
+        {
+            get;
+
+            set;
+        }
+        public void MakeNoise()
+        {
+
+            Console.WriteLine($"{Name} мяукает");
+
+        }
+        public int GetAge()
+        {
+            return (DateTime.Today - Berthday).Days / 365;
+
+
+        }
+    }
+}
