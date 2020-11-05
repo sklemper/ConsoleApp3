8d62f49bc99486d85cc98c5edf853448040d4f9d
Danil <danilsonia621@gmail.com>
2020-11-05 12:24:51 +1000

    3.11.2020

--- ConsoleApp3/cat.cs
+++ ConsoleApp3/cat.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-    class Cat
+    public class Cat
     {
         public Cat(string name, DateTime berthday)
         {
 
             Name = name;
-            Berthday = berthday;
+            BerthDay = berthday;
+            Task.Run(LifeCircle);
 
 
         }
@@ -21,23 +23,84 @@ namespace ConsoleApp3
 
             set;
         }
-        public DateTime Berthday
-        {
-            get;
-
-            set;
-        }
         public void MakeNoise()
         {
 
             Console.WriteLine($"{Name} мяукает");
+        }
+        public DateTime BerthDay
+        {
+            get;
+
+            set;
 
         }
         public int GetAge()
         {
-            return (DateTime.Today - Berthday).Days / 365;
+            return (DateTime.Today - BerthDay).Days / 365;
 
+        }
+        public byte HungryStatus
+        {
+            get { return HungryStatus; }
+            set
+            {
+                if (value < 0)
+                {
+                    HungryStatus = 0;
+                }
+                else if (value > 100)
+                {
+                    HungryStatus = 100;
+                }
+                else
+                    HungryStatus = value;
+            }
+        }
+        public void Getstatus()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст:{GetAge()}");
+            if (HungryStatus <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кошка умерает от голода:(");
+            }
+            else if (HungryStatus > 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Кошка очень голодна:");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Кошка хочет кушать :");
+            }
+            else if (HungryStatus > 70 && HungryStatus <= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Кошка не против перекусить:");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Кошка недавно поела :(");
+            }
+            Console.ResetColor();
 
+        }
+        async Task LifeCircle()
+        {
+            await Task.Delay(1000);
+            HungryStatus -= 10;
+            Getstatus();
+            if (HungryStatus == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Кошка умерла:(");
+            }
+            else
+                await LifeCircle();
         }
     }
 }
