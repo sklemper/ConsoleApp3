b9ae9dd36dc661a9c2ffee008bf58da255750ea3
sklemper <73625476+sklemper@users.noreply.github.com>
2020-11-13 13:47:03 +1000

    Add files via upload

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp3;
+using System;
 
 namespace ConsoleApp4
 {
@@ -8,10 +9,24 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Cat c = new Cat("Мурзик", new DateTime(2009, 04, 29));
+            Cat c = new Cat("Kit", new DateTime(2005, 8, 7));
             c.MakeNoise();
-            Console.WriteLine($"Коту {c.Name} уже {c.GetAge()} лет");
+            Console.WriteLine($"Кошке по имени {c.Name} уже {c.GetAge()} лет");
+            //c.HungryStatus = 150; 
+
+            Cat ct = new Cat($"Cry", new DateTime(2006, 9, 8));
+            ct.MakeNoise();
+            Console.WriteLine($"Кошке по имени {ct.Name} уже {ct.GetAge()} лет");
+
+
+            CatSmartHouse catSmartH = new CatSmartHouse(900);
+            catSmartH.AddCat(c);
+            catSmartH.AddCat(ct);
+            Console.ReadLine();
         }
+
+
+
     }
 
 }
